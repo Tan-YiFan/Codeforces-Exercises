42135dbc6650106ad4fa55f00a7b2584c985e4c3
Tan-YiFan <15711657500@163.com>
2021-04-20 10:54:35 +0800

    1514 C: Another approach

--- 1514/c/c.cpp
+++ 1514/c/c.cpp
@@ -38,63 +38,91 @@ int mod_reverse(int a, int n)
 		return -1;
 	}
 }
+// int main()
+// {
+// 	cin >> n;
+// 	if (n == 2) {
+// 		printf("1\n1\n");
+// 		return 0;
+// 	}
+// 	int cnt = 1;
+// 	ans[1] = true;
+// 	if (0) {
+// 		for (int i = 2; i <= n - 2; i++) {
+// 			cnt++;
+// 			ans[i] = true;
+// 		}
+// 	} else {
+// 		ll last = 0;
+// 		for (ll i = 2; i < n; i++) {
+// 			if (ans[i]) {
+// 				continue;
+// 			}
+// 			ll m = mod_reverse(i, n);
+// 			if (m == -1) {
+// 				continue;
+// 			} else if (ans[m]) {
+// 				continue;
+// 			} else if (m == i) {
+// 				if (last == 0) {
+// 					last = i;
+// 				} else {
+// 					ll l = last * i;
+// 					l %= n;
+// 					/* cnt += 3;
+// 					ans[last] = ans[i] = ans[l] = true;
+// 					last = 0; */
+// 					if (ans[l]) {
+// 						cnt++;
+// 						ans[l] = false;
+// 						ans[last] = ans[i] = true;
+// 						last = l;
+// 					} else {
+// 						cnt += 3;
+// 						ans[l] = ans[last] = ans[i] = true;
+// 						last = 0;
+// 					}
+// 				}
+// 			} else {
+// 				cnt += 2;
+// 				ans[i] = ans[m] = true;
+// 			}
+// 		}
+// 	}
+// 	cout << cnt << endl;
+// 	for (int i = 1; i <= n; i++) {
+// 		if (ans[i]) {
+// 			printf("%d ", i);
+// 		}
+// 	}
+// 	puts("");
+// 	return 0;
+// }
+
 int main()
 {
 	cin >> n;
-	if (n == 2) {
-		printf("1\n1\n");
-		return 0;
+	vector<int> out;
+	ll p = 1;
+	for (int i = 1; i <= n; i++) {
+		if (__gcd(i, n) == 1) {
+			p *= i;
+			p %= n;
+			out.push_back(i);
+		}
 	}
-	int cnt = 1;
-	ans[1] = true;
-	if (0) {
-		for (int i = 2; i <= n - 2; i++) {
-			cnt++;
-			ans[i] = true;
+	if (p == 1) {
+		printf("%d\n", out.size());
+		for (int i = 0; i < out.size(); i++) {
+			printf("%d ", out[i]);
 		}
 	} else {
-		ll last = 0;
-		for (ll i = 2; i < n; i++) {
-			if (ans[i]) {
-				continue;
-			}
-			ll m = mod_reverse(i, n);
-			if (m == -1) {
-				continue;
-			} else if (ans[m]) {
-				continue;
-			} else if (m == i) {
-				if (last == 0) {
-					last = i;
-				} else {
-					ll l = last * i;
-					l %= n;
-					/* cnt += 3;
-					ans[last] = ans[i] = ans[l] = true;
-					last = 0; */
-					if (ans[l]) {
-						cnt++;
-						ans[l] = false;
-						ans[last] = ans[i] = true;
-						last = l;
-					} else {
-						cnt += 3;
-						ans[l] = ans[last] = ans[i] = true;
-						last = 0;
-					}
-				}
-			} else {
-				cnt += 2;
-				ans[i] = ans[m] = true;
+		printf("%d\n", out.size() - 1);
+		for (int i = 0; i < out.size(); i++) {
+			if (out[i] != p) {
+				printf("%d ", out[i]);
 			}
 		}
 	}
-	cout << cnt << endl;
-	for (int i = 1; i <= n; i++) {
-		if (ans[i]) {
-			printf("%d ", i);
-		}
-	}
-	puts("");
 	return 0;
 }
